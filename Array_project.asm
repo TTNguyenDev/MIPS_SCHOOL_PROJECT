748b659b344c217d9f66dc8642c09b6f7a508963
TTNguyen <nttriet9a4@gmail.com>
2018-12-04 10:19:53 +0700

    output

--- Array_project.asm
+++ Array_project.asm
@@ -4,19 +4,21 @@
 .data
 	array: .word 100
 	
-	Result: .asciiz "\nKet qua: "
         Space: .asciiz " "
         Enter: .asciiz "\n"
         GetNumOfElement: .asciiz "\nNhap vao su lua chon cua ban: "
         GetSizeOfArray: .asciiz "\nNhap vao so luong phan tu: "
         ShowOption: .asciiz "\n----------------MENU----------------\n1. Xuat ra cac phan tu\n2. Tinh tong cac phan tu\n3. Liet ke cac phan tu la so nguyen to\n4. Max\n5. Find element\n6. Exit"
- 
+ 	Result: .asciiz "\nKet qua: "
+ 	comma: .asciiz  ", "
         
 .text
+
 .globl main
 main:
-	j getSize
-		
+	jal getSize
+	j EXIT
+
 
 getSize: 
 	la $a0, GetSizeOfArray
@@ -29,7 +31,7 @@ getSize:
 	move $s0, $v0 #s0 register store size of array	
 	move $t0, $s0 #create a copy of $s0
 	la $s1, array
-	j getInput
+	jal getInput
 
 
 getInput:
@@ -45,16 +47,16 @@ getInput:
 
 MENU: 
 	#Print menu 
-	addi $v0, $0, 4
+	li $v0, 4
 	la $a0, ShowOption
 	syscall
 	
-	addi $v0, $0, 4
+	li $v0, 4
 	la $a0, GetNumOfElement
 	syscall
 	
 	#Nhap so nguyen
-	addi $v0, $0, 5
+	li $v0, 5
 	syscall
 	
 	#Luu tru so nguyen vua nhap vao $t0
@@ -63,9 +65,39 @@ MENU:
 	bgt $t0, 6, MENU
 	blez $t0, MENU
 	
+	
+	
+	
+	jal EXIT
+	
+output: 
+
+	la $a0, Result
+	li, $v0, 4
+	syscall
+	la $s1, array
+	move $t0, $s0
+	
+loop:
+	beq $t0, 0, MENU
+	
+	lw $t2, ($s1)
+	move $a0, $t2
+	li, $v0, 1
+	syscall
+	
+	la $a0, comma
+	li, $v0, 4
+	syscall
+	
+	addi $t0, $t0, -1
+	addi $s1, $s1, 4
+	
+	b loop
+	
 EXIT: 
 	#Ket thuc chuong trinh
 	addi $v0, $0 10
-	syscall
+	syscall 
 
 
